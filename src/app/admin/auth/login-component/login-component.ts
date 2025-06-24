c94a6390df8f5c0753762aae8fcf6fbaef87b270
Manish Sharma <sharmakmanish1@gmail.com>
2025-06-24 17:08:34 +0530

    routes added - basic

--- src/app/admin/auth/login-component/login-component.ts
+++ src/app/admin/auth/login-component/login-component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+   loginForm: FormGroup;
+  private fb: FormBuilder = inject(FormBuilder);
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      // Handle login logic here
+      console.log('Username:', username);
+      console.log('Password:', password);
+    }
+  }
+}
